b3c2712fa08fb890d8dbbc2bc3eedf2923934126
Haider Ali Sehar <haideralisehar254@gmail.com>
2025-09-04 16:21:16 +0500

    Add

--- src/app/components/LanguageSwitcher.jsx
+++ src/app/components/LanguageSwitcher.jsx
@@ -28,7 +28,7 @@ export default function LanguageSwitcher() {
       translate="no"
       onClick={toggleLanguage}
       style={{
-        padding: "10px 16px",
+        padding: "11px 18px 8px 12px",
         color: "black",
         border: "none",
         borderRadius: "6px",
