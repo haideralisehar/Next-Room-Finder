23711b44bd23e5dd1509e0a6a1a0f1372e9c2143
Haider Ali Sehar <haideralisehar254@gmail.com>
2025-09-07 13:27:57 +0500

    Add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,11 @@
 import "./globals.css";
 import GoogleTranslateLoader from "../app/components/GoogleTranslateLoader";
 import LanguageSwitcher from "../app/components/LanguageSwitcher";
+import {Jost} from "next/font/google";
+
+const jost = Jost({
+  subsets:['latin']
+})
 
 export default function RootLayout({ children }) {
   return (
@@ -10,7 +15,7 @@ export default function RootLayout({ children }) {
         <meta name="description" content="This is my awesome Next.js application" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body>
+      <body className={jost.className}>
         {/* Load hidden Google widget */}
         <GoogleTranslateLoader />
 
