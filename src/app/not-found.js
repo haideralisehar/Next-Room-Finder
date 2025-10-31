b5ef35862ab1c76e22eea616b3e55b3f208b8b2c
Haider Ali Sehar <haideralisehar254@gmail.com>
2025-10-31 23:07:19 +0500

    Add

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,27 @@
+"use client"
 import React from 'react'
-
+import Lottie from "lottie-react";
+import notfound from "../lotti-img/notfound.json";
 export default function NotFound() {
   return (
     <div>
-      not found
+      <div
+  style={{
+    display: "flex",
+    justifyContent: "center",   // horizontal center
+    alignItems: "center",       // vertical center
+    height: "100vh",            // full screen height
+    width: "100vw",             // full screen width
+  }}
+>
+  <Lottie
+    style={{ width: "100%", height: "70%" }}
+    animationData={notfound}
+  />
+</div>
+
+
+      
     </div>
   )
 }
