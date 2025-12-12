ca9e753ed1de8a5867e67dd705e9691a4c432843
Haider Ali Sehar <haideralisehar254@gmail.com>
2025-12-13 01:22:40 +0500

    Addd

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,11 +2,6 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
-module.exports = {
-  experimental: {
-    serverComponentsExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
-  },
-};
 
 
 export default config;
